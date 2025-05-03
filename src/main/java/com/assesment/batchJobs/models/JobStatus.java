afd528757123df5d558693466148b014a72bfeb2
Aissaoui Moetaz <84248599+MoetazAissaoui@users.noreply.github.com>
2025-05-04 00:40:30 +0100

    Tech Assessment - cleaning.

--- src/main/java/com/assesment/batchJobs/models/JobStatus.java
+++
@@ -1,5 +0,0 @@
-package com.assesment.batchJobs.models;
-
-public enum JobStatus {
-    ACTIVE, PAUSED, CANCELLED
-}
--- src/main/java/com/assesment/batchJobs/models/ScheduleType.java
+++
@@ -1,5 +0,0 @@
-package com.assesment.batchJobs.models;
-
-public enum ScheduleType {
-    CRON, FIXED_RATE
-}
