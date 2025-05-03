fe746bd33e740b976a8a5a26b3ceff2eacb80957
Aissaoui Moetaz <84248599+MoetazAissaoui@users.noreply.github.com>
2025-05-04 00:39:05 +0100

    Tech Assessment - Enabling Scheduling mode and parametrization of continues pooling & logging level.

--- src/main/java/com/assesment/batchJobs/BatchJobsApplication.java
+++ src/main/java/com/assesment/batchJobs/BatchJobsApplication.java
@@ -2,8 +2,10 @@ package com.assesment.batchJobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BatchJobsApplication {
 
 	public static void main(String[] args) {
